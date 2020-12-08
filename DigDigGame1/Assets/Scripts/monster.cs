410bf02ca0817852e7f04897a0b69c5b3edfb796
ElmerCarlfalk <elmer.carlfalk@elev.ga.lbs.se>
2020-12-08 16:06:19 +0100

    lektion 2
    Lade in knappar och monster fas 1, kopplade knappar till health bar

---
+++ DigDigGame1/Assets/Scripts/monster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster : MonoBehaviour
+{
+    public int maxHealth;
+    private int currentHealth;
+
+    public health healthBar;
+
+    public int CurrentHealth { get => currentHealth; set => currentHealth = value; }
+
+    void Start()
+    {
+        CurrentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentHealth <= 0)
+        {
+            Object.Destroy(this.gameObject);
+            Object.Destroy(healthBar.gameObject);
+        }
+    }
+    public void TakeDamage()
+    {
+        currentHealth = currentHealth - 20;
+        healthBar.SetHealth(currentHealth);
+    }
+    public void GetHealth()
+    {
+        currentHealth = currentHealth + 15;
+        healthBar.SetHealth(currentHealth);
+    }
+}
